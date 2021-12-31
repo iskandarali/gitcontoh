8dc5d74d07a615f611f88c8a30d39e701d2b76b5
Mohd Iskandar Mohd Ali <m.iskandar@moh.gov.my>
2021-12-31 17:17:28 +0800

    install breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Gittest</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    </head>
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
     <body>
-        <h1>Hello World!</h1>
-        <p>Ini adalah cubaan pertama <br/> Ini adalah cubaan kedua</p>
+        <div class="container">
+            <h1>Hello World!</h1>
+            <p>Ini adalah cubaan pertama <br /> Ini adalah cubaan kedua</p>
+            <p>Ini adalah cubaan ketiga</p>
+        </div>
     </body>
 </html>
